09cea008244827500c284f2342bf6530ee82245e
JW9506 <cf.dota@gmail.com>
2023-12-28 22:10:56 -0600

    init

---
+++ src/main/java/com/quickstart/_streambasic/Main.java
@@ -0,0 +1,15 @@
+package com.quickstart._streambasic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+    }
+}
