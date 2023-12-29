3efc2710cbe68b891328af32f4f833de937d351e
JW9506 <cf.dota@gmail.com>
2023-12-28 23:27:00 -0600

    Refactor

--- src/main/java/com/quickstart/_IOStreambasic/ByteRead.java
+++ src/main/java/com/quickstart/_IOStreambasic/ByteRead.java
@@ -8,10 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 public class ByteRead {
   public static void main(String[] args) throws IOException {
     try (FileInputStream in = new FileInputStream("input.txt");) {
-      byte[] bytes = new byte[3];
+      byte[] buffer = new byte[3];
       int len;
-      while ((len = in.read(bytes)) != -1) {
-        log.info("Reading: {}", new String(bytes, 0, len));
+      while ((len = in.read(buffer)) != -1) {
+        log.info("Reading: {}", new String(buffer, 0, len));
       }
     }
   }
