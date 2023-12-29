3ebdc196a2301d3d1d96058ab3fc5d3873fa1b1c
JW9506 <cf.dota@gmail.com>
2023-12-28 22:50:39 -0600

    File Stream

---
+++ src/main/java/com/quickstart/_IOStreambasic/File_Input_Stream.java
@@ -0,0 +1,24 @@
+package com.quickstart._IOStreambasic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Slf4j
+public class File_Input_Stream {
+  public static void main(String[] args) {
+    try (FileInputStream in = new FileInputStream("input.txt");
+        FileOutputStream out = new FileOutputStream("output.txt")) {
+
+      int c;
+      while ((c = in.read()) != -1) {
+        out.write(c);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
+
